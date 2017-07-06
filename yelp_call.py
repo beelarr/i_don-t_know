54a0c0a6e449ce6a8f8a241165735bb4e4c5e31e
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-06 12:20:59 -0500

    Inital commit with out yelp credentials

---
+++ yelp_call.py
@@ -0,0 +1,36 @@
+
+
+def get_search_params(lat, long, open_now):
+    params = {}
+    params['ll'] = '{},{}'.format(str(lat), str(long))
+    params['open'] = open_now == True
+    params['limit'] = '1'
+
+    return params
+
+def get_results(params):
+    consumer_key = ''
+    consumer_secret=''
+    token = ''
+    token_secret=''
+
+    session = rauth.OAuth1Session(
+        consumer_key = consumer_key,
+        consumer_secret = consumer_secret,
+        access_token = token,
+        access_token_secret = token_secret
+    )
+
+    request = session.get('http://api.yelp.com')
+
+    #Transforms the JSON API response into a dict
+
+    data = request.json()
+    session.close
+
+    return data
+
+def main():
+    locations=[#pull from yelp as user location]
+    api_calls = []
+    time.sleep(1.0)
